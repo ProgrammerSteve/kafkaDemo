1db01c6bbe97699a76a6468ee5920365c3f6e583
Steven <stevenmb1995@gmail.com>
2024-10-15 20:58:37 -0500

    edited conda env for python, and added some validation. Added requests for go server

--- TextualTUI/app.py
+++ TextualTUI/app.py
@@ -3,6 +3,8 @@ from textual.app import App
 from textual.widgets import Footer, Header, Button, Static, Label, Input
 from textual.validation import Number
 from textual.containers import ScrollableContainer
+import requests
+import math
 
 
 class NumberInput(Static):
@@ -38,18 +40,21 @@ class ProducerApp(App):
         print("pressed")
         input_widget = self.query_one("#producer-number-input-1", Input)
         input_value = input_widget.value
-        if input_widget.type=='text':
-            input_widget.value=''
-        if input_widget.type=='number':
-            input_widget.value=0.0
-        if input_widget.type=='integer':
-            input_widget.value=0
+
         try:
-            value = float(input_value) # Validate and convert the input value to a float
-            self.produced_value = value  # Update the produced_value directly in the app
+            value = int(float(input_value))
+            self.produced_value = value
+            input_widget.value=""
             print(f"Produced value updated to: {self.produced_value}")
         except ValueError:
-            print("Invalid input: Please enter a valid number")
+            self.produced_value = math.nan
+            print(f"Invalid input: value set to NaN")
+
+        if not math.isnan(self.produced_value):
+            response = requests.post('http://localhost:8081/send', json={'number': str(self.produced_value)})
+            print("Response:", response.status_code, response.json())
+        else:
+            print("Cannot send NaN value to the server.")
 
 if __name__ == "__main__":
     ProducerApp().run()
